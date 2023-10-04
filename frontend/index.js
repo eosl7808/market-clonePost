1a12a5817c30fc59366a475530413d8495e5c953
HYEONGYEOMWOO <108976686+eosl7808@users.noreply.github.com>
2023-10-04 19:23:57 +0900

    로그인 인증 기능 추가

--- frontend/index.js
+++ frontend/index.js
@@ -23,10 +23,10 @@ const calcTime = (timestamp) => {
   else return "방금 전";
 };
 
-//reverse()로 최신기준 내림차순으로 바꿔줌 
+//reverse()로 최신기준 내림차순으로 바꿔줌
 const renderData = (data) => {
   const main = document.querySelector("main");
-  data.reverse().forEach(async(obj) => {
+  data.reverse().forEach(async (obj) => {
     const div = document.createElement("div");
     div.className = "item-list";
 
@@ -34,10 +34,10 @@ const renderData = (data) => {
     imgDiv.className = "item-list__img";
 
     const img = document.createElement("img");
-    const res= await fetch(`/images/${obj.id}`);
-    const blob=await res.blob();
-    const url= URL.createObjectURL(blob);
-    
+    const res = await fetch(`/images/${obj.id}`);
+    const blob = await res.blob();
+    const url = URL.createObjectURL(blob);
+
     img.src = url;
 
     const InfoDiv = document.createElement("div");
@@ -68,7 +68,18 @@ const renderData = (data) => {
 };
 
 const fetchList = async () => {
-  const res = await fetch("/items");
+  const accessToken = window.localStorage.getItem("token");
+  const res = await fetch("/items", {
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+  if (res.status === 401) {
+    alert("로그인이 필요합니다.");
+    window.location.pathname = "/login.html";
+    return;
+  }
+
   const data = await res.json();
   renderData(data);
 };
